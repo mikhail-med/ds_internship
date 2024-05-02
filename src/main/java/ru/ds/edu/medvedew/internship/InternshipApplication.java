e54035553c42a96ee428cad2a968131291818f00
mikhail <mikhail.medvedew@mail.ru>
2024-05-02 20:51:36 +0300

    db init
    and add new in pom.xml

--- src/main/java/ru/ds/edu/medvedew/internship/InternshipApplication.java
+++ src/main/java/ru/ds/edu/medvedew/internship/InternshipApplication.java
@@ -2,12 +2,12 @@ package ru.ds.edu.medvedew.internship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class InternshipApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(InternshipApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InternshipApplication.class, args);
+    }
 
 }
